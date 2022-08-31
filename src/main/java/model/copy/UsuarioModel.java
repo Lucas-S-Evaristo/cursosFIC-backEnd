6d0992d4db85946de12c5427c2fb42878fc3a286
João Pedro Gomes da Silva <112516752+joaopedro1764@users.noreply.github.com>
2022-08-31 16:05:24 -0300

    BD

---
+++ src/main/java/model/copy/UsuarioModel.java
@@ -0,0 +1,13 @@
+package model.copy;
+
+import javax.persistence.Entity;
+
+@Entity
+public class UsuarioModel {
+
+	private int id;
+	private String nome;
+	private String email;
+	private String nif;
+	
+}
