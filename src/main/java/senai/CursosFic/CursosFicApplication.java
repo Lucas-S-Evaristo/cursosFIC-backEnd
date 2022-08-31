3c042b68088af2a9d3046611aee6f3a0fbe6a2bd
Lucas Evaristo <lucasev35@gmail.com>
2022-08-31 14:47:44 -0300

    Commit inicial

---
+++ src/main/java/senai/CursosFic/CursosFicApplication.java
@@ -0,0 +1,13 @@
+package senai.CursosFic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosFicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosFicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/senai/CursosFic/CursosFicApplicationTests.java
@@ -0,0 +1,13 @@
+package senai.CursosFic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosFicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
